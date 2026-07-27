6038af1782848dd0b4dc5f936beafe382ea8b9a2
name <email@gmail.com>
2026-07-27 11:44:11 +0300

    Создание новой ветки. Добавление линтеров в проект, их настройка. Исправление стилей и отступов с помощью линтеров.

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 """
-    1. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+1. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
